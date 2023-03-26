08c8bcfeef643c28319d08398f9bee6d7a1cc577
Mykhailo Zbihlei <mac@MacBook-Pro.local>
2023-03-26 15:07:33 +0300

    react-redux-toolkit

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+// import {filtersFetching,filtersFetched, filtersFetchingError} from '../components/heroesFilters/filtersSlice';
+
+// export const fetchFilters = (request) =>(dispatch) =>{
+//     dispatch(filtersFetching());
+//     request("http://localhost:3001/filters")
+//         .then(data => dispatch(filtersFetched(data)))
+//         .catch(() => dispatch(filtersFetchingError()))
+// }
